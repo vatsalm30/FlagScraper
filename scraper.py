900502b57312e604f8bc024fd86161e0f74d26d9
vatsalm30 <vatsalm30@gmail.com>
2024-06-18 17:48:36 -0400

    finished

--- scraper.py
+++ scraper.py
@@ -1,10 +1,13 @@
-import requests  
-from bs4 import BeautifulSoup  
-    
+import requests
+from bs4 import BeautifulSoup
+import urllib.request
+from urllib.parse import urlparse, unquote
+
+
 def getdata(url):  
     r = requests.get(url)  
     return r.text  
-    
+
 htmldata = getdata("https://vexillology.fandom.com/wiki/List_of_national_capital_flags")  
 soup = BeautifulSoup(htmldata, 'html.parser')  
 for item in soup.find_all('img', attrs={'class':'thumbimage'}): 
@@ -12,5 +15,5 @@ for item in soup.find_all('img', attrs={'class':'thumbimage'}):
     position = imgurl.find('/revision')
     if position != -1:
         imgurl = imgurl[:position]
-    item["src"] = imgurl
-    print(item["src"])
+    
+    urllib.request.urlretrieve(imgurl, unquote(urlparse(imgurl).path.rsplit('/', 1)[-1]))
